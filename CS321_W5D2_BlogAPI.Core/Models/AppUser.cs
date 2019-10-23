4d662ee31454e78f6a525972ad358378659728cc
Osman Samad <osamad84@yahoo.com>
2019-10-22 19:02:46 -0500

    just a check.

--- CS321_W5D2_BlogAPI.Core/Models/AppUser.cs
+++ CS321_W5D2_BlogAPI.Core/Models/AppUser.cs
@@ -7,8 +7,17 @@ namespace CS321_W5D2_BlogAPI.Core.Models
     public class AppUser : IdentityUser
     {
         public string FirstName { get; set; }
+        
+        
         public string LastName { get; set; }
         // TODO: add a FullName property that returns FirstName + LastName
+        public string FullName
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
         public ICollection<Blog> Blogs { get; set; }
     }
 }
